b9e742b0d6898eedf529df920533b70ad534c1a5
Hasibullah Mahmood <48029545+HasibullahMahmood@users.noreply.github.com>
2019-05-13 10:43:26 +0300

    Update hw2.sh

--- hw2_lab7/hw2.sh
+++ hw2_lab7/hw2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Read MBR and write it into a file.
-# hd -n 512 /dev/sda > MyMbr.txt
+hd -n 512 /dev/sda > MyMbr.txt
 
 # Enter a new entry in GRUB with an iso under your home directory.
 # https://www.howtogeek.com/196933/how-to-boot-linux-iso-images-directly-from-your-hard-drive/
 
-# echo 'menuentry "ubuntu-18.10-desktop-amd64.iso" {
-#	set isofile="/home/hasibullah/ubuntu-18.10-desktop-amd64.iso"
-#	loopback loop (hd0,5)$isofile
-#	linux (loop)/casper/vmlinuz boot=casper iso-scan/filename=${isofile} quiet splash
-#	initrd (loop)/casper/initrd
-#	}' >> /etc/grub.d/40_custom
-# update-grub
-# reboot
+echo 'menuentry "ubuntu-18.10-desktop-amd64.iso" {
+set isofile="/home/hasibullah/ubuntu-18.10-desktop-amd64.iso"
+loopback loop (hd0,5)$isofile
+linux (loop)/casper/vmlinuz boot=casper iso-scan/filename=${isofile} quiet splash
+initrd (loop)/casper/initrd
+}' >> /etc/grub.d/40_custom
+update-grub
+reboot
